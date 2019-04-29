183e0f40e88e1fe4759db98395d47b30394fd10e
edmondchui <chui.hinwai@gmail.com>
2019-04-28 19:49:43 -0600

    *some noop change to try azure auto deploy

--- src/components/email-array/email-button.js
+++ src/components/email-array/email-button.js
@@ -5,7 +5,7 @@ import React from "react";
 const EmailButton = ({name = '', src = '', href = ''}) => (
   <Button color="primary" href={href} target="_blank" rel="noopener noreferrer">
     <img className="left-icon button-img" width={24} height={24} src={src} alt={name}/>
-    {name}
+    <span>{name}</span>
   </Button>
 );
 

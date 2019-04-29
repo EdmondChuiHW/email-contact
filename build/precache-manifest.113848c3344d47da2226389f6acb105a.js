23496e6e07b08332cb7393c7f91403277d34be03
chuihinwai <chui.hinwai+azure.pipeline@gmail.com>
2019-04-29 02:18:47 +0000

    Automated Release 1 with Azure

---
+++ build/precache-manifest.113848c3344d47da2226389f6acb105a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9796ed786d95606d51be9dab54fb5350",
+    "url": "/email-contact/static/media/yahoo.9796ed78.ico"
+  },
+  {
+    "revision": "759fade9033aa298629e4b000dcd6dde",
+    "url": "/email-contact/static/media/outlook.759fade9.ico"
+  },
+  {
+    "revision": "2db0d88cea7a3cef82deba04d4c9f354",
+    "url": "/email-contact/static/media/gmail.2db0d88c.ico"
+  },
+  {
+    "revision": "db528bf7f61c1f5a3895",
+    "url": "/email-contact/static/js/runtime~main.3d4a06f5.js"
+  },
+  {
+    "revision": "b28898bcfb6509da619a",
+    "url": "/email-contact/static/js/main.5ecffb86.chunk.js"
+  },
+  {
+    "revision": "40a40ef6db0020803f87",
+    "url": "/email-contact/static/js/2.ae4db52c.chunk.js"
+  },
+  {
+    "revision": "b28898bcfb6509da619a",
+    "url": "/email-contact/static/css/main.d676e1eb.chunk.css"
+  },
+  {
+    "revision": "7b6af764fff2918ca2121ea2291ac585",
+    "url": "/email-contact/index.html"
+  }
+];

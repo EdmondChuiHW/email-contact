f43cdb12bf8207e9c7a54b74fb79fd9dba1921df
edmondchui <chui.hinwai@gmail.com>
2019-06-03 01:34:52 -0600

    +add ramda eslint [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
     'react-app',
     'plugin:cypress/recommended',
     'airbnb',
+    'plugin:ramda/recommended',
   ],
   'rules': {
     'react/jsx-filename-extension': 'off',
   },
   'plugins': [
     'cypress',
+    'ramda',
   ],
   'env': {
     'cypress/globals': true,

594978dd1f3784c7788cb15c70891d6a19c03741
edmondchui <chui.hinwai@gmail.com>
2019-06-05 19:33:25 -0600

    +clears query spec

--- src/hoc/withDebouncedQueryChange.test.js
+++ src/hoc/withDebouncedQueryChange.test.js
@@ -9,24 +9,43 @@ jest.useFakeTimers();
 afterEach(cleanup);
 
 // eslint-disable-next-line react/prop-types
-const DumbSearchBar = ({ onQueryChange }) => (
-  <input onChange={onQueryChange} />
+const DumbSearchBar = ({ onQueryChange, onClearQuery }) => (
+  <>
+    <input onChange={onQueryChange} />
+    <button onClick={onClearQuery} type="button" />
+  </>
+);
+
+const debounceInMs = 500;
+const makeDebouncedSearchBar = () => {
+  const SearchBar = withPropsHistory(DumbSearchBar);
+  const DebouncedSearchBar = withDebouncedQueryChange({ debounceInMs })(SearchBar);
+  return [DebouncedSearchBar, SearchBar.propsHistory];
+};
+
+const typeQuery = ({ text, inContainer }) => (
+  fireEvent.change(inContainer.querySelector('input'), { target: { value: text } })
+);
+
+const clickClearButton = ({ inContainer }) => (
+  fireEvent.click(inContainer.querySelector('button'), {})
 );
 
 describe('withDebouncedQueryChange', () => {
   it('calls onQueryChange only once after debounce interval', async () => {
-    const debounceInMs = 500;
-    const DebouncedSearchBar = withDebouncedQueryChange({ debounceInMs })(DumbSearchBar);
+    const [DebouncedSearchBar] = makeDebouncedSearchBar();
 
     const onQueryChange = jest.fn();
     const { container } = render(
       <DebouncedSearchBar onQueryChange={onQueryChange} />,
     );
-    const input = container.querySelector('input');
-    fireEvent.change(input, { target: { value: 'l' } });
-    fireEvent.change(input, { target: { value: 'la' } });
-    fireEvent.change(input, { target: { value: 'las' } });
-    fireEvent.change(input, { target: { value: 'last' } });
+
+    [
+      'l',
+      'la',
+      'las',
+      'last',
+    ].forEach(text => typeQuery({ text, inContainer: container }));
 
     jest.advanceTimersByTime(debounceInMs * 2);
 
@@ -35,44 +54,36 @@ describe('withDebouncedQueryChange', () => {
   });
 
   it('sets query prop every time', async () => {
-    const debounceInMs = 500;
-    const SearchBar = withPropsHistory(DumbSearchBar);
-    const DebouncedSearchBar = withDebouncedQueryChange({ debounceInMs })(SearchBar);
+    const [DebouncedSearchBar, propsHistory] = makeDebouncedSearchBar();
 
     const { container } = render(<DebouncedSearchBar />);
 
-    const input = container.querySelector('input');
-    fireEvent.change(input, { target: { value: 'l' } });
-    fireEvent.change(input, { target: { value: 'la' } });
-    fireEvent.change(input, { target: { value: 'las' } });
-    fireEvent.change(input, { target: { value: 'last' } });
+    [
+      'l',
+      'la',
+      'las',
+      'last',
+    ].forEach(text => typeQuery({ text, inContainer: container }));
 
     jest.advanceTimersByTime(debounceInMs * 2);
 
-    const history = SearchBar.propsHistory;
     const initialRenderAndEventsCount = 1 + 4;
-    expect(history.length).toEqual(initialRenderAndEventsCount);
-    expect(last(history).query).toEqual('last');
+    expect(propsHistory.length).toEqual(initialRenderAndEventsCount);
+    expect(last(propsHistory).query).toEqual('last');
   });
 
   it('clears query', async () => {
-    const debounceInMs = 500;
-    const SearchBar = withPropsHistory(DumbSearchBar);
-    const DebouncedSearchBar = withDebouncedQueryChange({ debounceInMs })(SearchBar);
+    const [DebouncedSearchBar, propsHistory] = makeDebouncedSearchBar();
+    const onQueryChange = jest.fn();
 
-    const { container } = render(<DebouncedSearchBar />);
+    const { container } = render(<DebouncedSearchBar onQueryChange={onQueryChange} />);
 
-    const input = container.querySelector('input');
-    fireEvent.change(input, { target: { value: 'l' } });
-    fireEvent.change(input, { target: { value: 'la' } });
-    fireEvent.change(input, { target: { value: 'las' } });
-    fireEvent.change(input, { target: { value: 'last' } });
+    typeQuery({ text: 'some', inContainer: container });
+    clickClearButton({ inContainer: container });
 
     jest.advanceTimersByTime(debounceInMs * 2);
 
-    const history = SearchBar.propsHistory;
-    const initialRenderAndEventsCount = 1 + 4;
-    expect(history.length).toEqual(initialRenderAndEventsCount);
-    expect(last(history).query).toEqual('last');
+    expect(onQueryChange).toBeCalledTimes(1);
+    expect(last(propsHistory).query).toEqual('');
   });
 });

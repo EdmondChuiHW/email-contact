afde1c48ff26b210a1bb921066c99f240470b070
edmondchui <chui.hinwai@gmail.com>
2020-02-15 19:04:51 -0700

    update tests

--- src/utils/makeYegCoreZoneEmail.test.js
+++ src/utils/makeYegCoreZoneEmail.test.js
@@ -9,11 +9,11 @@ describe('makeYegCoreZoneEmail', () => {
     });
     const expected = `Hello Councillor McKeen,
 
-I want safe and liveable streets. That means 30 km/hr in residential areas. The #YEGCoreZone is the only option to move Edmonton in this direction.
+I want safe and liveable streets. That means 30 km/hr for residential streets in Edmonton. The #YEGCoreZone is the only option being presented to get us there. Please vote to approve the #YEGCoreZone on February 26.
 
-Please vote to approve the #YEGCoreZone on February 26. 
+Please also consider voting to reduce speed limits throughout the rest of Edmonton, in addition to the 30 km/hr Core Zone. It's time for Edmonton's residential streets to become the vibrant, community-building spaces our neighbourhoods and their residents would like to see. Vote YES to the Core Zone on February 26 and YES to slower streets in Edmonton's neighbourhoods.
 
-Thank you,
+Sincerely,
 Constituent from Edmonton`;
     expect(actual[0]).toEqual(expected);
   });
@@ -26,11 +26,11 @@ Constituent from Edmonton`;
     });
     const expected = `Hello Councillor McKeen,
 
-I want safe and liveable streets. That means 30 km/hr in residential areas. The #YEGCoreZone is the only option to move Edmonton in this direction.
+I want safe and liveable streets. That means 30 km/hr for residential streets in Edmonton. The #YEGCoreZone is the only option being presented to get us there. Please vote to approve the #YEGCoreZone on February 26.
 
-Please vote to approve the #YEGCoreZone on February 26. 
+Please also consider voting to reduce speed limits throughout the rest of Edmonton, in addition to the 30 km/hr Core Zone. It's time for Edmonton's residential streets to become the vibrant, community-building spaces our neighbourhoods and their residents would like to see. Vote YES to the Core Zone on February 26 and YES to slower streets in Edmonton's neighbourhoods.
 
-Thank you,
+Sincerely,
 Constituent from Ward 6`;
     expect(actual[0]).toEqual(expected);
   });
@@ -39,11 +39,11 @@ Constituent from Ward 6`;
     const actual = makeYegCoreZoneEmail({});
     const expected = `Hello Mayor Iveson,
 
-I want safe and liveable streets. That means 30 km/hr in residential areas. The #YEGCoreZone is the only option to move Edmonton in this direction.
+I want safe and liveable streets. That means 30 km/hr for residential streets in Edmonton. The #YEGCoreZone is the only option being presented to get us there. Please vote to approve the #YEGCoreZone on February 26.
 
-Please vote to approve the #YEGCoreZone on February 26. 
+Please also consider voting to reduce speed limits throughout the rest of Edmonton, in addition to the 30 km/hr Core Zone. It's time for Edmonton's residential streets to become the vibrant, community-building spaces our neighbourhoods and their residents would like to see. Vote YES to the Core Zone on February 26 and YES to slower streets in Edmonton's neighbourhoods.
 
-Thank you,
+Sincerely,
 Constituent from Edmonton`;
     expect(actual[0]).toEqual(expected);
   });

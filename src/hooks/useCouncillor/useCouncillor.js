a2fa53cb80ff4082a95694542c76d7a683c260cc
Edmond Chui <edmond@MBP.local>
2020-02-23 23:58:32 -0700

    lint

--- src/hooks/useCouncillor/useCouncillor.js
+++ src/hooks/useCouncillor/useCouncillor.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { pipe } from 'ramda';
 import { getCouncillorFromAddress, getCouncillorFromGeo } from '../../services/address2councillor.service';
 
@@ -6,10 +6,10 @@ const useCouncillor = ({ address = '', coords = { latitude: 0, longitude: 0 } })
   const [councillor, setCouncillor] = useState(undefined);
   const [completedQuery, setCompletedQuery] = useState(undefined);
 
-  const onCouncillorLoaded = pipe(
+  const onCouncillorLoaded = useMemo(() => pipe(
     setCouncillor,
     () => setCompletedQuery(address),
-  );
+  ), [setCouncillor, setCompletedQuery, address]);
 
   useEffect(() => {
     const req = coords && !address
@@ -17,7 +17,7 @@ const useCouncillor = ({ address = '', coords = { latitude: 0, longitude: 0 } })
       : getCouncillorFromAddress(address);
     req.then(onCouncillorLoaded);
     return () => req.abort();
-  }, [address, coords]);
+  }, [address, coords, onCouncillorLoaded]);
   return [councillor, completedQuery];
 };
 

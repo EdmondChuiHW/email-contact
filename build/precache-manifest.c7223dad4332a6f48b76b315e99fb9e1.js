febdb5d62f9d74957218f41399ccac023dc180b5
EdmondChuiHW <chui.hinwai+azure.pipeline@gmail.com>
2020-11-20 19:09:19 +0000

    Automated Release 28 with Azure https://dev.azure.com/EdmondChuiHW/fa893097-e9a2-4456-8ddf-b7d60e3a52cf/_release?releaseId=28&_a=release-summary

---
+++ build/precache-manifest.c7223dad4332a6f48b76b315e99fb9e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f716412ba56cf820e025f6667fe7e71",
+    "url": "/email-contact/index.html"
+  },
+  {
+    "revision": "ef194c39dc36577df6eb",
+    "url": "/email-contact/static/css/main.b0f856d4.chunk.css"
+  },
+  {
+    "revision": "7701d60d541085130177",
+    "url": "/email-contact/static/js/2.080288a9.chunk.js"
+  },
+  {
+    "revision": "36d82ddad827ebf3af1c21481884e623",
+    "url": "/email-contact/static/js/2.080288a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef194c39dc36577df6eb",
+    "url": "/email-contact/static/js/main.8bab8561.chunk.js"
+  },
+  {
+    "revision": "6a1848434627ce3b1eaa",
+    "url": "/email-contact/static/js/runtime-main.6acd5fb1.js"
+  },
+  {
+    "revision": "2db0d88cea7a3cef82deba04d4c9f354",
+    "url": "/email-contact/static/media/gmail.2db0d88c.ico"
+  },
+  {
+    "revision": "759fade9033aa298629e4b000dcd6dde",
+    "url": "/email-contact/static/media/outlook.759fade9.ico"
+  },
+  {
+    "revision": "9796ed786d95606d51be9dab54fb5350",
+    "url": "/email-contact/static/media/yahoo.9796ed78.ico"
+  }
+]);

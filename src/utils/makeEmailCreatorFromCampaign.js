5554e385f2183eed5c7ac8d97016efe114f24066
Edmond Chui <chui.hinwai@gmail.com>
2021-04-13 03:37:24 +0100

    Update makeEmailCreatorFromCampaign.js

--- src/utils/makeEmailCreatorFromCampaign.js
+++ src/utils/makeEmailCreatorFromCampaign.js
@@ -3,8 +3,7 @@ export default function makeEmailCreatorFromCampaign({ body, subject }) {
   return function emailCreator({ role = 'Mayor', lastName = 'Iveson', ward = '' }) {
     const textBody = `Hello ${role} ${lastName},
 
-${body}
-Constituent from ${ward || 'Edmonton'}`;
+${body}`;
 
     return [textBody, subject];
   };
